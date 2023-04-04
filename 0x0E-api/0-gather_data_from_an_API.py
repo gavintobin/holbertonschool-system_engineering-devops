1cf0edcbfa17329d747ae9095cf320e53d50a65c
gavintobin <gavintobin714@gmail.com>
2023-04-04 14:46:35 -0700

    try1

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -1,46 +1,38 @@
 #!/usr/bin/python3
-
+"""
+Script that returns information about an employees TODO list progress
+given an employee id.
+"""
+import json
 import requests
 import sys
-import urllib
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    num_done, num_tasks = 0, 0
+    userId = sys.argv[1]
 
-    # Define the base URL for the API
-    base_url = 'https://jsonplaceholder.typicode.com'
+    # create Response object for specific user and that user's tasks
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(userId)
+    user_response = requests.get(url)
 
-    # Define the employee ID as a command-line argument
-    employee_id = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/todos/?userId={}'\
+        .format(userId)
+    todo_response = requests.get(url)
 
-    # Define the endpoint URL for the employee's TODO list
-    todo_url = f'{base_url}/todos?userId={employee_id}'
+    # create Dictionary objects from response objects
+    user_info = json.loads(user_response.text)
+    todo_info = json.loads(todo_response.text)
 
-    # Send a GET request to the TODO list endpoint
-    response = requests.get(todo_url)
+    employee_name = user_info['name']
 
-    # Parse the JSON response into a Python dictionary
-    todos = response.json()
+    for task in todo_info:
+        num_tasks += 1
+        if task['completed']:
+            num_done += 1
 
-    # Count the number of completed tasks and the total number of tasks
-    num_completed_tasks = 0
-    total_num_tasks = len(todos)
-    completed_tasks = []
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, num_done, num_tasks))
 
-    for todo in todos:
-        if todo['completed']:
-            num_completed_tasks += 1
-            completed_tasks.append(todo['title'])
-
-    # Get the employee's name
-    user_url = f'{base_url}/users/{employee_id}'
-    user_response = requests.get(user_url)
-    user_data = user_response.json()
-    employee_name = user_data['name']
-
-    # Display the TODO list progress
-    print('Employee {} is done with tasks({}/{}):'
-          .format(employee_name, num_completed_tasks, total_num_tasks))
-
-    # Display the completed task titles
-    for task in completed_tasks:
-        print(f"\t {task}")
+    for task in todo_info:
+        if task['completed']:
+            print("\t {}".format(task['title']))
